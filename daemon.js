a207a8bea863360f17908074e26b829ea44ba39f
Fabian Hauser <fabian@fh2.ch>
2016-09-07 16:32:18 +0200

    Remove ES6 lambdas from daemon

--- daemon.js
+++ daemon.js
@@ -2,12 +2,17 @@
 
 var process = require('process');
 var Clockdaemon = require('./app/clockdaemon');
+
+// initialize the clock running daemon
 var daemon_clock = new Clockdaemon('./config.json');
 
-process.on('exit', (code) => {
+// initialize webserver
+// TODO
+
+process.on('exit', function(code) {
 	daemon_clock.cleanup();
 });
 
-process.on('SIGUSR2', () => {
+process.on('SIGUSR2', function() {
 	daemon_clock.reload();
 });
