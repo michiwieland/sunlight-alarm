560417d6ab72a1c72f49a81f42908d52273796a1
Fabian Hauser <fabian@fh2.ch>
2016-09-07 12:53:20 +0000

    Add configurator

---
+++ src/configurator.js
@@ -0,0 +1,23 @@
+'use strict';
+
+var fs = require('fs');
+
+var Configurator = (function () {
+    function Configurator(file) {
+        this.file = file;
+    }
+
+    // Methods
+    Configurator.prototype.readConfig = function(){
+	return JSON.parse(fs.readFileSync(this.file));
+    }
+
+    Configurator.prototype.writeConfig = function(data) {
+	var dataString = JSON.stringify(data);
+	return fs.writeFileSync(this.file, dataString);
+    }
+
+    return Configurator;
+})();
+
+exports = module.exports = Configurator;
