911055d00b3fc63d9566b3425d13cc243cf21c9e
Fabian Hauser <fabian@fh2.ch>
2016-09-07 13:30:05 +0000

    Add clockdaemon

---
+++ daemon.js
@@ -0,0 +1,13 @@
+'use strict';
+
+var process = require('process');
+var Clockdaemon = require('./src/clockdaemon');
+var daemon_clock = new Clockdaemon('./config.json');
+
+process.on('exit', (code) => {
+	daemon_clock.cleanup();
+});
+
+process.on('SIGUSR2', () => {
+	daemon_clock.reload();
+});
---
+++ src/clockdaemon.js
@@ -0,0 +1,46 @@
+'use strict';
+
+var schedule = require('node-schedule');
+var Configurator = require('./configurator');
+var LightController = require('./lightcontroller');
+
+
+var Clockdaemon = (function(){
+	function Clockdaemon(configfile){
+		this.configurator = new Configurator(configfile);
+		this.reload();
+		this.alarms = [];
+	}
+
+	Clockdaemon.prototype.cleanup = function(){
+		this.deleteSchedulingTasks();
+	};
+
+	Clockdaemon.prototype.loadConfiguration = function(){
+		this.config = this.configurator.readConfig();
+	};
+
+	Clockdaemon.prototype.deleteSchedulingTasks = function(){
+		// TODO: unschedule
+	};
+
+	Clockdaemon.prototype.createSchedulingTasks = function(){
+		for(var alarm of config.alarms){
+			this.alarms.push(schedule.scheduleJob(alarm.time, function(){
+				var light = new LightController();
+				light.enlighten();
+				setTimeout(alarm.light.timeout, function(){
+					light.off();
+				};
+			});
+		}
+	};
+
+	Clockdaemon.prototype.reload = function(){
+		this.loadConfiguration();
+		this.deleteSchedulingTasks();
+		this.createSchedulingTasks();
+	};
+}
+
+exports = module.exports = Clockdaemon;
