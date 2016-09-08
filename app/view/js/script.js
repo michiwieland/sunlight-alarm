deabd5b69bff5e15d38b9fb675a301d4ae8d7c50
michiwieland <michi.wieland@hotmail.com>
2016-09-08 08:36:21 +0200

    validation updated

--- app/view/js/script.js
+++ app/view/js/script.js
@@ -8,25 +8,6 @@ requirejs(["time"]);
 	var lightIntervall = document.getElementById("light-intervall");
 	var weekDays = document.getElementsByName("weekday");
 
-	// POST form data to server
-	function validateForm() {
-		var timeRegex = /^([0-1]?[0-9]{1}|2[0-3]{1}):[0-5]{1}[0-9]{1}$/;
-		var minRegex = /^[0-5]{1}[0-9]{1}$/;
-
-		var timeValue = timeInput.value;
-		var intervalValue = lightIntervall.value;
-
-		if ( !timeRegex.test(timeValue) ) {
-			return false;
-		}
-		if ( intervalValue != "" ) {
-			if ( !minRegex.test(intervalValue) ) {
-				return false;
-			}
-		}
-		return true;
-	}
-
 	// serialize form
 	function serializeForm() {
 		var alarm = new Alarm();
@@ -52,6 +33,7 @@ requirejs(["time"]);
 
 		var postData = serializeForm();
 
+		// debug
 		console.log(postData)
 
 		request.send(postData);
@@ -59,10 +41,8 @@ requirejs(["time"]);
 
 // Ready Event
 document.addEventListener("DOMContentLoaded", function(event) {
-	timeForm.addEventListener("change", function(event) {
-		if (validateForm()) {
-			writeConfiguration();
-		}
+	timeForm.addEventListener("submit", function(event) {
+		writeConfiguration();
 	});
 
 });
