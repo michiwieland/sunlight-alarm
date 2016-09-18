72555fb2dd3fc188a12b90a4cbfe24b90ddedeea
Fabian Hauser <fabian@fh2.ch>
2016-09-18 18:12:36 +0200

    Implement lightcontroller options

--- app/lightcontroller.js
+++ app/lightcontroller.js
@@ -7,8 +7,52 @@ var LightController = (function(){
 		this.controllerBinary = './bin/send-milight-cmd';
 	}
 
-	LightController.prototype.enlighten = function(){
-		// TODO: Graceful
+	LightController.prototype.sendCommand = function(command){
+		child_process.execFile(this.controllerBinary, [ command ]);
+	};
+
+
+	// combined operations
+
+	LightController.prototype.enlighten = function(duration){
+		// Enable Night Mode
+		this.nightmode();
+
+		for(var i=1; i<14; i++) {
+			setTimeout(i * duration/13 * 1000, function(){
+				this.on();
+				this.lighten();
+			});
+		}
+	};
+
+	LightController.prototype.lighten = function(){
+		this.sendCommand("5A AC 39 00 04 06 50");
+	};
+
+	LightController.prototype.dim = function(){
+		this.sendCommand("5A AC 39 00 14 E8 42");
+	};
+
+	LightController.prototype.dimToLowest = function() {
+		for(var i=1; i<14; i++) {
+			this.dim();
+		}
+	};
+
+	// light temperature
+
+	LightController.prototype.colder = function(){
+		this.sendCommand("5A AC 39 00 0F 55 AA");
+	};
+
+	LightController.prototype.warmer = function(){
+		this.sendCommand("5A AC 39 00 0E 52 A6");
+	};
+
+	// basic actions
+
+	LightController.prototype.on = function(){
 		this.sendCommand("5A AC 39 00 05 B3 F7");
 	};
 
@@ -16,8 +60,9 @@ var LightController = (function(){
 		this.sendCommand("5A AC 39 00 09 B3 F7");
 	};
 
-	LightController.prototype.sendCommand = function(command){
-		child_process.execFile(this.controllerBinary, [ command ]);
+	LightController.prototype.nightmode = function(){
+		this.sendCommand("5A AC 39 00 05 FF 4A");
+		this.sendCommand("5A AC 39 00 09 00 4F");
 	};
 
 	return LightController;
