e07954839277aa6a5996d48d2008918ad0b98b3f
Fabian Hauser <fabian@fh2.ch>
2016-10-19 15:48:51 +0200

    Rewrite Webserver as class

---
+++ app/webserver.js
@@ -0,0 +1,64 @@
+'use strict';
+var Configurator = require('./configurator');
+
+var express = require('express');
+var path = require('path');
+
+var Webserver = (function(){
+	function Webserver(daemon) {
+		this.daemon = daemon;
+		this.app = express();
+		this.server = null;
+
+		this.setMiddleware();
+		this.setRoutes();
+		this.start();
+	}
+
+	Webserver.prototype.setMiddleware = function() {
+		// body parser for POST requests
+		var bodyParser = require('body-parser')
+		this.app.use(bodyParser.json() );
+		this.app.use(bodyParser.urlencoded({
+		  extended: true
+		}));
+
+		// static serving
+		this.app.use(express.static(path.join(__dirname, 'view')));
+		this.app.use(express.static(path.join(__dirname, 'model')));
+	}
+
+	Webserver.prototype.setRoutes = function() {
+		// Save configurations
+		this.app.post('/saveConfiguration', function(req, res){
+			var configurator = new Configurator("configuration.json");
+			configurator.writeConfig(req.body);
+			this.daemon.reload();
+			res.status(200).send({success: true});
+		}.bind(this));
+
+		// Load all available alarm configurations
+		this.app.get('/loadConfigurations', function(req, res){
+			var configurator = new Configurator("configuration.json");
+			res.json(configurator.readConfig());
+		}.bind(this));
+	}
+
+	Webserver.prototype.start = function() {
+
+
+		// Define the port to run on
+		this.app.set('port', 3000);
+
+
+		// Listen for requests
+		this.server = this.app.listen(this.app.get('port'), function() {
+		  var port = this.server.address().port;
+		  console.log('Magic happens on port ' + port);
+		}.bind(this));
+	}
+
+	return Webserver;
+})();
+
+module.exports = Webserver;
